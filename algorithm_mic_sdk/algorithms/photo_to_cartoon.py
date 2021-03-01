090a3345f9df26ced19a84de3b169672da1b8b36
潘荣锁 <panrs@venpoo.com>
2021-03-01 16:10:55 +0800

    人像转卡通算法增加美颜参数

--- algorithm_mic_sdk/algorithms/photo_to_cartoon.py
+++ algorithm_mic_sdk/algorithms/photo_to_cartoon.py
@@ -6,17 +6,22 @@ from ..tools import FileInfo
 class PhotoToCartoon(AlgoBase):
     __algo_name__ = 'photo_to_cartoon'
 
-    def __init__(self, auth_info: AuthInfo, file: FileInfo, process=None, custom_data=None, **kwargs):
+    def __init__(self, auth_info: AuthInfo, file: FileInfo, process=None, beauty_level=None, need_cache=True,
+                 custom_data=None, **kwargs):
         """
         人像转卡通算法
             文档见 https://www.yuque.com/fenfendeyouzhiqingnian/algorithm/kz1mpu
         @param auth_info:个人权限配置参数
         @param file:文件对象,FileInfo对象
         @param process:图片缩放参数
+        @param need_cache:是否使用缓存
+        @param beauty_level:美颜参数
         @param custom_data:自定义参数,将会随着响应参数原样返回
         """
         super().__init__(auth_info)
         self.request['oss_file'] = file.get_oss_name(self)
         self.request['process'] = process
         self.request['custom_data'] = custom_data
+        self.request['need_cache'] = need_cache
+        self.request['beauty_level'] = beauty_level
         self.request.update(kwargs)
