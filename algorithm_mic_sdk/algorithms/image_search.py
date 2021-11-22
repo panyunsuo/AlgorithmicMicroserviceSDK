d95e5684868d76ba108aac1276665d4055ccf5aa
潘荣锁 <panrs@venpoo.com>
2021-11-22 11:39:21 +0800

    新增图片搜索算法

---
+++ algorithm_mic_sdk/algorithms/image_search.py
@@ -0,0 +1,20 @@
+from ..auth import AuthInfo
+from ..base import AlgoBase
+from ..tools import FileInfo
+
+
+class ImageSearch(AlgoBase):
+    __algo_name__ = 'image_search'
+
+    def __init__(self, auth_info: AuthInfo, file: FileInfo, custom_data=None, **kwargs):
+        """
+        物体检测算法
+            文档见 https://www.yuque.com/fenfendeyouzhiqingnian/algorithm/yv7anc
+        @param auth_info:个人权限配置参数
+        @param file:文件对象,FileInfo对象
+        @param custom_data:自定义参数,将会随着响应参数原样返回
+        """
+        super().__init__(auth_info)
+        self.request['oss_file'] = file.get_oss_name(self)
+        self.request['custom_data'] = custom_data
+        self.request.update(kwargs)
